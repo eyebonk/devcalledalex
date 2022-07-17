a19614d0adc7146de5cc72d72ee8122713c5605c
Alex Smith <eyebonk@gmail.com>
2022-07-18 06:49:49 +1000

    config test added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <h1>app.vue</h1>
+  {{ TEST }}
 </template>
 
 <script>
 // import HelloWorld from "@components/HelloWorld.vue";
+import { TEST } from "@config/experience.js";
 
 export default {
   components: {
     // HelloWorld,
+  },
+  setup() {
+    return {
+      TEST,
+    };
   },
 };
 </script>
