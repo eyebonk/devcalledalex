906a022d50482c40ed16b9caa0771ac5e585bd37
Alex <eyebonk@gmail.com>
2024-04-16 07:48:29 +1000

    Add comps added

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,24 @@
 <script setup lang="ts">
-// import MyExperience from '@components/MyExperience.vue'
-// import OtherProjects from '@components/OtherProjects.vue'
+import MyExperience from '@components/experience/MyExperience.vue'
+import OtherProjects from '@components/projects/OtherProjects.vue'
 import AboutMe from '@components/AboutMe.vue'
 import MySkills from '@components/MySkills.vue'
 import MySocial from '@components/MySocial.vue'
-// import TheBackgrounds from '@components/TheBackgrounds.vue'
+import MyProjects from '@components/projects/MyProjects.vue'
+import TheBackgrounds from '@components/TheBackgrounds.vue'
+import ThemeButtons from '@components/ThemeButtons.vue'
 import { usePresets } from '@composables/usePresets.ts'
 import type { PRESET } from '@models/Preset.ts'
-import { PRESETS } from '@config/colors'
 import { onMounted } from 'vue'
 
 const {
-  isRetroActive,
   isGreenActive,
-  changePreset,
   setDefaults,
-  presetType,
 } = usePresets()
 
 onMounted(() => {
   setDefaults()
 })
-
-function changeType(type: PRESET) {
-  changePreset(type)
-}
-
-function isActive(type: string) {
-  return type === presetType.value ? 'tw-bg-pink' : 'tw-bg-green'
-}
 </script>
 
 <template>
@@ -45,34 +35,13 @@ function isActive(type: string) {
 
       <AboutMe />
       <MySkills />
-      <!-- <MyExperience />
+      <MyExperience />
       <MyProjects />
-      <OtherProjects /> -->
+      <OtherProjects />
     </div>
 
-    <!-- <TheBackgrounds /> -->
-
-    <div
-      class="tw-fixed tw-bottom-2 tw-right-2 tw-p-2 tw-flex tw-flex-col tw-space-y-2 tw-z-50"
-    >
-      <button
-        v-for="(item, index) in PRESETS"
-        :key="index"
-        class="tw-h-8 tw-w-8 tw-rounded-lg tw-text-sm tw-text-off-black"
-        :class="isActive(item.type)"
-        @click="changeType(item)"
-      >
-        <font-awesome-icon :icon="item.icon" />
-      </button>
-    </div>
-
-    <div v-if="isRetroActive" class="tw-text-sm tw-opacity-0">
-      <a
-        href="https://www.freepik.com/free-vector/retro-futuristic-landscape-background-with-sun_5072279.htm#query=80s%20background&position=0&from_view=keyword&track=ais"
-      >
-        Image by pikisuperstar</a>
-      on Freepik
-    </div>
+    <TheBackgrounds />
+    <ThemeButtons />
   </div>
 </template>
 
