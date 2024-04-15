b780fbaab240f9fa77e58bc1c995a40688c3e791
Alex <eyebonk@gmail.com>
2024-04-16 07:47:11 +1000

    Projects and projects item

---
+++ src/components/projects/MyProjects.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ProjectItems from '@components/projects/ProjectItems.vue'
+import BaseHeader from '@components/BaseHeader.vue'
+import { MY_PROJECTS } from '@config/projects.js'
+</script>
+
+<template>
+  <div>
+    <BaseHeader text="My projects" />
+
+    <ProjectItems :items="MY_PROJECTS" />
+  </div>
+</template>
