5dd07a08e33eda1962dd37dd7ce52f7f076116a5
Alex <eyebonk@gmail.com>
2024-04-17 14:03:19 +1000

    Remove old folder

--- OLD/src/composables/index.js
+++
@@ -1,2 +0,0 @@
-export { default as usePresets } from "./usePresets.js";
-export { default as useFilter } from "./useFilter.js";
--- OLD/src/main.js
+++
@@ -1,9 +0,0 @@
-import FontAwesomeIcon from "@plugins/fontawesome.js";
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./css/app.css";
-
-const app = createApp(App);
-
-app.component("FontAwesomeIcon", FontAwesomeIcon);
-app.mount("#app");
