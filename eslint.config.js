231923ba32d8bea14f55ee20b06d9ece6a893a3a
Alex Smith <eyebonk@gmail.com>
2025-12-22 11:22:35 +0000

    Linting update

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,18 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({ ignores: ['**/public/*.js'] })
+export default antfu({
+  ignores: [
+    '.vscode/*',
+    '.git/*',
+    '.github/*',
+    'dist/*',
+    'node_modules/*',
+    'public/*',
+    '*.md',
+    '*/**.md',
+  ],
+  rules: {
+    // Allow process.env usage in server files
+    'node/prefer-global/process': 'off',
+  },
+})
