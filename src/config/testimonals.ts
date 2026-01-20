90f6eeed279ec2cc02fb08166e3fb4d2b827cdd8
Alex Smith <eyebonk@gmail.com>
2026-01-20 20:04:48 +0000

    fix fonts

---
+++ src/config/testimonals.ts
@@ -0,0 +1,17 @@
+export const SOCIAL = [
+  {
+    name: 'Reece Alexander',
+    company: 'mod.io',
+    text: 'Working with Alex was truly inspiring, educational and extremely insightful. Master of all things frontend Alex continued to blow me away time and time again by taking some of the most complex concepts I\'ve seen in my two decades of experience in this industry and effortlessly turning it into the most simplistic yet elegant and beautiful frontend components.',
+  },
+  {
+    name: 'Abdullah Quazi',
+    company: 'Australian Defence of Defence',
+    text: 'Alex and I have been working together for quite some time on one of the really big and complex projects I have ever worked on so far. And here is the thing about Alex, whenever I needed some changes only to suit my end he would come up with a good solution, not a dirty fix but trying best to cover the future needs. What can you say about a dev who says, "Quazi, I had a dream about the problem we were talking about and I solved the problem in my dream". He is talented, dedicated, focused, easygoing and I can say from my experience he can be asset to any team.',
+  },
+  {
+    name: 'Wayne Hewitt',
+    company: 'IOStudios',
+    text: 'Alex is a great asset to have in any team. We’ve worked on many big websites together and Alex isn’t afraid of picking up the slack or go beyond the call of duty for the client needs. Alex is easy to get on with and is always willing to learn new skills.',
+  },
+]
